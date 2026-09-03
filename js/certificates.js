17f2b738e48450cd93ebe34156da99204c2ef9a2
jadhavm1472-sys <jadhavm1472@gmail.com>
2026-09-03 22:59:10 +0530

    Create certificates.js

---
+++ js/certificates.js
@@ -0,0 +1,65 @@
+const certificates = {
+
+    "45024950096": {
+
+        certificateNumber: "४५०२४९५००९६",
+
+        certificateName: "विवाह नोंद दाखला",
+
+        applicantName: "किशोर रामसिंग खडकी",
+
+        gramsevakName: "के. वाय खडकी",
+
+        issueDate: "२५-०८-२०२६",
+
+        gramPanchayat: "दहेगाव",
+
+        taluka: "बागलाण",
+
+        district: "नाशिक"
+
+    },
+
+
+    "45024950097": {
+
+        certificateNumber: "४५०२४९५००९७",
+
+        certificateName: "जन्म दाखला",
+
+        applicantName: "राम पाटील",
+
+        gramsevakName: "के. वाय खडकी",
+
+        issueDate: "२६-०८-२०२६",
+
+        gramPanchayat: "दहेगाव",
+
+        taluka: "बागलाण",
+
+        district: "नाशिक"
+
+    },
+
+
+    "45024950098": {
+
+        certificateNumber: "४५०२४९५००९८",
+
+        certificateName: "रहिवासी दाखला",
+
+        applicantName: "सुरेश पाटील",
+
+        gramsevakName: "के. वाय खडकी",
+
+        issueDate: "२७-०८-२०२६",
+
+        gramPanchayat: "दहेगाव",
+
+        taluka: "बागलाण",
+
+        district: "नाशिक"
+
+    }
+
+};
